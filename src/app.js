60d04c93311f618cdb9f5e9575a9611148c6f7dd
luiza-paloschi <luizapaloschi@gmail.com>
2023-01-07 19:12:12 -0300

    projeto sem bônus

--- src/app.js
+++ src/app.js
@@ -12,30 +12,30 @@ const tweets = [];
 app.post("/sign-up", (req, res) => {
     const user = req.body;
     users.push(user);
-    res.send("OK!");
+    res.status(201).send("OK");
 })
 
 app.post("/tweets", (req, res) => {
     const tweet = req.body;
     const signedIn = users.find(user => user.username === tweet.username);
     if (!signedIn){
-        console.log("aaaaaaaa")
-        return res.send("UNAUTHORIZED")
+        return res.sendStatus(401);
     }
+
     tweets.push(tweet);
-    res.send("OK!");
-})
+    res.status(201).send("OK");
+});
 
 app.get("/tweets", (_, res) => {
     if (tweets.length === 0){
         return res.send(tweets);
     }
 
-    const newTweets = tweets.map((item) => { 
-       return {...item, avatar: users[0].avatar};
+    const newTweets = tweets.map((tweet) => {
+        const user = users.find((user) => user.username === tweet.username);
+       return {...tweet, avatar: user.avatar};
     })
-    const limited = newTweets.filter((item, index) => index > newTweets.length -11);
-    console.log(limited)
+    const limited = newTweets.filter((_, index) => index > newTweets.length -11);
     res.send(limited);
 })
 
